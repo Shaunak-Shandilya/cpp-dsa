868f0cb9e1e10dd10ac920787f830092405136a3
Shaunak-Shandilya <shaunak.shandilya@outlook.com>
2025-08-01 17:21:21 +0530

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,42 +1,175 @@
-//BUBBLE SORT
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
 
-//INSERTION SORT
-/*
-time & space complexity = O(n^2)
-adaptive
-stable
-better performance on linked list than on array
-*/
-void insertionsort(int *arr, int size) {
-	//int* arr = (int*)malloc(size * sizeof(int));
-	//int* arr = new int[size];
-	int i, j, x;
+using namespace std;
 
-	for (i = 1; i < size; i++) {
-		j = i - 1;
-		x = arr[i];
-		while (j > -1 && arr[j] > x) {
-			arr[j + 1] = arr[j];
-			j--;
-		}
-		arr[j + 1];
+void display(vector<int> temp) {
+	for(int i : temp){
+		printf("%d ", i);
+		printf("\n");
 	}
 }
-
-//SELECTION SORT
-/*
-time and space complexity O(n^2)
-not stable (original order of duplicates is lost), not adaptive
-*/
-
+void insertionSort(vector<float>& bucket) {
+    for (int i = 1; i < bucket.size(); ++i) {
+        float key = bucket[i];
+        int j = i - 1;
+        while (j >= 0 && bucket[j] > key) {
+            bucket[j + 1] = bucket[j];
+            j--;
+        }
+        bucket[j + 1] = key;
+    }
+}
 void selectionsort(int *arr, int size) {
 	int i,j,k;
 	for (i = 0; i < size-1; i++) {
 		for (j = k = i; j < size; j++) {
-			if (arr[j] < arr[k]) {
+			if (arr[j] < arr[k])	 {
 				k = j;
 			}
 			std::swap(arr[i], arr[k]);
 		}
 	}
+}
+
+int partition(vector<int>& arr, int low, int high) {
+
+	int pivot = arr[high];
+	int i = low - 1;
+	for (int j = low; j <= high - 1; j++) {
+		if (arr[j] < pivot) {
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[high]);
+	return i + 1;
+}
+void quickSort(vector<int>& arr, int low, int high) {
+
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void merge(vector<int>& arr, int left,
+    int mid, int right) {
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+void countSort(int arr[], int n, int exp)
+{
+    int* output = new int[n];
+    int i, count[10] = { 0 };
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+    delete[] output;
+}
+void radixsort(int arr[], int n)
+{
+    int m = getMax(arr, n);
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+void bucketSort(float arr[], int n) {
+    // 1) Create n empty buckets
+    vector<vector<float>> b(n);
+
+    // 2) Put array elements in different buckets
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i];
+        if (bi >= n) bi = n - 1; // Prevent out-of-bounds
+        b[bi].push_back(arr[i]);
+    }
+
+    // 3) Sort individual buckets using insertion sort
+    for (int i = 0; i < n; i++) {
+        insertionSort(b[i]);
+    }
+
+    // 4) Concatenate all buckets into arr[]
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < b[i].size(); j++) {
+            arr[index++] = b[i][j];
+        }
+    }
+}
+
+int main() {
+	vector<int> temp = { 20,10,5,60,50,70,45,32767 };
+	int n = 8, i;
+	//quickSort(temp, 0, n-1);
+	
+	
+	display(temp);
+	
 }
